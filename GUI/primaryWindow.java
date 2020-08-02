36cedcd9a8804532b887183d35e4c55fb78e943c
Philip Lewander <46770041+tnuggi@users.noreply.github.com>
2020-08-02 13:33:31 -0500

    open JavaFX library works
    the class that main method is housed in cannot extend application. It messes everything up.

--- GUI/primaryWindow.java
+++ GUI/primaryWindow.java
@@ -1,11 +1,20 @@
 package GUI;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class PrimaryWindow extends Application {
 
-public class primaryWindow{
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Primary Window"); 
+ 
+        
 
+        StackPane root = new StackPane();
+        primaryStage.setScene(new Scene(root, 300, 250));
 
-    public primaryWindow() {
-
+        primaryStage.show();
     }
 }
--- Main.java
+++ Main.java
@@ -1,36 +1,11 @@
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import GUI.PrimaryWindow; 
 
-public class Main extends Application {
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(PrimaryWindow.class);
     }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Project Carna");
-        Button btn = new Button();
-        btn.setText("Say 'HelloWorld'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent arg0) {
-                System.out.println("Hello World");
-
-            }
-
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300,250));
-        primaryStage.show();
-    }
+ 
+    
 }
